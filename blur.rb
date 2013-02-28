a01bf94b1241a17a539a0a1875f17d020f7e1ac0
Brian Anderson <banderson@mozilla.com>
2013-02-27 19:49:19 -0800

    Initial

---
+++ blur.rb
@@ -0,0 +1,78 @@
+require 'json'
+require 'logger'
+require 'sinatra'
+require 'base64'
+require 'matrix'
+
+get '/' do
+  redirect '/index.html'
+end
+
+post '/blur/ruby' do
+  blur { |w, h, d| blur_ruby(w, h, d) }
+end
+
+post '/blur/rust' do
+  blur { |w, h, d| blur_rust(w, h, d) }
+end
+
+def blur
+  msg = request.body.read
+  msg = JSON.parse msg
+
+  width = msg['width']
+  height = msg['height']
+  data = msg['data']
+
+  newdata = blur_ruby(width, height, data)
+
+  response = { :data => newdata }
+  JSON.generate(response)
+end
+
+def blur_ruby(width, height, data)
+
+  def pixel(width, height, data, x, y)
+    data[width * (y % height) + (x % width)]
+  end
+
+  mat = Matrix.rows([[0.1, 0.1, 0.1],
+                     [0.1, 0.2, 0.1],
+                     [0.1, 0.1, 0.1]])
+
+  newdata = []
+
+  0.upto (height - 1) do |y|
+    0.upto (width - 1) do |x|
+      # Calculate the pixel value
+      total = 0
+      0.upto mat.row_size - 1 do |yy|
+        0.upto mat.column_size - 1 do |xx|
+          pixel_value = pixel(width, height, data,
+                              x - (mat.row_size - 1) / 2 + xx,
+                              y - (mat.column_size - 1) / 2 + yy)
+          pixel_value = pixel_value * mat[yy, xx]
+          total = total + pixel_value
+        end
+      end
+      newdata[width * y + x] = total
+    end
+  end
+
+  newdata
+end
+
+def old_blur_ruby(width, height, data)
+  newdata = []
+  0.upto (height - 1) do |y|
+    0.upto (width - 1) do |x|
+      current = data[width * y + x]
+      newdata[width * y + x] = 255 - current
+    end
+  end
+  newdata
+end
+
+def blur_rust(width, height, data)
+  data
+end
---
+++ public/script.js
@@ -0,0 +1,87 @@
+$(document).ready(function() {
+
+    var drawing = false;
+    var mouseX = 0;
+    var mouseY = 0;
+    var canvas = $("#surface");
+    var canvas_offset = canvas.offset();
+    var context = canvas[0].getContext('2d');
+
+    // Initialize the canvas
+    (function initialize() {
+        var data = context.createImageData(canvas.width(), canvas.height());
+        for (x = 0; x < data.width * data.height; x++) {
+            data.data[x * 4 + 0] = 255;
+            data.data[x * 4 + 1] = 255;
+            data.data[x * 4 + 2] = 255;
+            data.data[x * 4 + 3] = 255;
+        }
+        context.putImageData(data, 0, 0);
+    })();
+
+    canvas.mousedown(function(e) {
+        mouseX = e.pageX - canvas_offset.left;
+        mouseY = e.pageY - canvas_offset.top;
+        drawing = true
+    });
+
+    canvas.mousemove(function(e) {
+        if (!drawing) { return; }
+
+        var startX = mouseX;
+        var startY = mouseY;
+        var endX = e.pageX - canvas_offset.left;
+        var endY = e.pageY - canvas_offset.top;
+        mouseX = endX;
+        mouseY = endY;
+
+        context.beginPath();
+        context.moveTo(startX, startY);
+        context.lineTo(endX, endY);
+        context.stroke();
+    });
+
+    canvas.mouseup(function() {
+        drawing = false;
+    });
+
+    function podifyImageData(data) {
+        var dataArray = [];
+        // For simplicity, just extract one channel
+        for (x = 0; x < data.width * data.height; x++) {
+            dataArray[x] = data.data[x * 4];
+        }
+
+        return {
+            width: data.width,
+            height: data.height,
+            data: dataArray
+        };
+    }
+
+    function blur(backend) {
+        var data = context.getImageData(0, 0, canvas.width(), canvas.height());
+        var data = podifyImageData(data);
+        var data = JSON.stringify(data);
+        $.post('/blur/' + backend, data, function(newData) {
+            var newData = JSON.parse(newData);
+            var data = context.createImageData(canvas.width(), canvas.height());
+            for (x = 0; x < data.width * data.height; x++) {
+                data.data[x * 4 + 0] = newData.data[x];
+                data.data[x * 4 + 1] = newData.data[x];
+                data.data[x * 4 + 2] = newData.data[x];
+                data.data[x * 4 + 3] = 255;
+            }
+            context.putImageData(data, 0, 0);
+        });
+    }
+
+    $("#ruby-blur").click(function() {
+        blur('ruby');
+    });
+
+    $("#rust-blur").click(function() {
+        blur('rust');
+    });
+
+});
